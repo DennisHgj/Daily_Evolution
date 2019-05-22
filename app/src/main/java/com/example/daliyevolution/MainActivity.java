1cf9a174ed1c750799ec82f2e0310c6145876314
CHAO ZHANG <u6545192@anu.edu.au>
2019-05-23 07:31:48 +0800

    mainactivity

--- app/src/main/java/com/example/daliyevolution/MainActivity.java
+++ app/src/main/java/com/example/daliyevolution/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.daliyevolution;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 /***
  * MainActivity
  * main activity
- * @author Guanjie Huang
+ * @author Guanjie Huang, Chao Zhang
  * @ID u6532079
+ * @ID u6545192
  */
 
 public class MainActivity extends AppCompatActivity {
@@ -15,5 +18,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onPress(View v){
+        EditText txt= findViewById(R.id.editText);
+        txt.setText("");
     }
 }
