dcaee6f49e413e359299fd7b6194797e8c96b9d0
CHAO ZHANG <u6545192@anu.edu.au>
2019-05-23 07:41:48 +0800

    Datas

---
+++ app/src/main/java/com/example/daliyevolution/util/Datas.java
@@ -0,0 +1,12 @@
+package com.example.daliyevolution.util;
+
+import java.util.ArrayList;
+/***
+ *
+ * mainactivity config
+ * @author Chao Zhang
+ * @ID u6545192
+ */
+public class Datas {
+    public static ArrayList<String> datas=new ArrayList<>();
+}
