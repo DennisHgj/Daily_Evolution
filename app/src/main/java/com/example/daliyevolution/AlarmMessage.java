3288e20c369b625dd620c5de372ff464ddae183d
CHAO ZHANG <u6545192@anu.edu.au>
2019-05-22 20:34:12 +0800

    alarm modified

--- app/src/main/java/com/example/daliyevolution/AlarmMessage.java
+++ app/src/main/java/com/example/daliyevolution/AlarmMessage.java
@@ -22,7 +22,7 @@ public class AlarmMessage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         new AlertDialog.Builder(this)//show the dialog
                 .setTitle("Time is up！")
-                .setMessage("Ring Ring Ring !!!：")
+                .setMessage(" Do it now!")
                 .setPositiveButton("Close", new DialogInterface.OnClickListener() {
 
                     @Override
