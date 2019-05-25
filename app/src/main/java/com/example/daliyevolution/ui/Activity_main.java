ee9fc778279e3d98310cc0c94b0bb9b2c088b92e
Lingyu Xia <u6483756@anu.edu.au>
2019-05-25 15:53:03 +1000

    add app info button at home page

--- app/src/main/java/com/example/daliyevolution/ui/Activity_main.java
+++ app/src/main/java/com/example/daliyevolution/ui/Activity_main.java
@@ -1,10 +1,14 @@
 package com.example.daliyevolution.ui;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.view.View;
 import android.widget.RadioGroup;
 
 import com.example.daliyevolution.R;
@@ -58,6 +62,46 @@ public class Activity_main extends BaseFragActivity {
     private void setListener() {
         myradiogroup.setOnCheckedChangeListener(new myOnCheckedChangeListener());
         myradiogroup.check(R.id.bt_home);
+    }
+
+    public void onAppinfoClick(View view) {
+        new AlertDialog.Builder(this)//show the dialog
+                .setTitle("AppInfo")
+                .setMessage(" Home Page :\n" +
+                        "\n" +
+                        "* Display on-time climate information\n" +
+                        "* Refresh automatically when starting the application\n" +
+                        "* Get real data from internet\n" +
+                        "* Could change the city and refresh\n" +
+                        "\n" +
+                        "Alarm Page :\n" +
+                        "* Display added alarms list\n" +
+                        "* Edit an alarm\n" +
+                        "* Add an alarm\n" +
+                        "* Delete an alarm\n" +
+                        "* Throw out alert information when time is up\n" +
+                        "\n" +
+                        "Notebook Page :\n" +
+                        "* Display added notes list\n" +
+                        "* Edit a note, when label and content are null, delete it\n" +
+                        "* Clear all notes\n" +
+                        "* Add a note \n" +
+                        "\n" +
+                        "Transaction Page :\n" +
+                        "* Transactions are divided into two part: Income and Spending. Every part has many types of transaction.\n" +
+                        "* Display transactions in different months\n" +
+                        "* Use Pie chart to display transactions category analysis in one month\n" +
+                        "* Edit a transaction\n" +
+                        "* Add a transaction\n" +
+                        "* Delete a transaction")
+                .setPositiveButton("Close", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        initFragment();
+                        setListener();
+                    }
+                }).show();
     }
 
     class myOnCheckedChangeListener implements RadioGroup.OnCheckedChangeListener {
