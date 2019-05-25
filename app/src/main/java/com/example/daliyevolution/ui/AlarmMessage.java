b3c75f1e4f86e2e1241d05bf2c2da3bef60b9938
Lingyu Xia <u6483756@anu.edu.au>
2019-05-25 16:04:26 +1000

    startactivity problem modified

--- app/src/main/java/com/example/daliyevolution/ui/AlarmMessage.java
+++ app/src/main/java/com/example/daliyevolution/ui/AlarmMessage.java
@@ -1,10 +1,14 @@
 package com.example.daliyevolution.ui;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.example.daliyevolution.fragment.Fragment_home;
+import com.example.daliyevolution.fragment.Fragment_time;
 
 /***
  * AlarmMessage
@@ -25,9 +29,9 @@ public class AlarmMessage extends AppCompatActivity {
 
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        Intent intent1 = new Intent(getApplicationContext(), Activity_main.class);
-                        intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                        startActivity(intent1);
+                        Intent intent = new Intent(AlarmMessage.this, Fragment_home.class);
+                        setResult(Activity.RESULT_OK, intent);
+                        finish();
                     }
                 }).show();
 
